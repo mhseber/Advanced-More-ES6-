6a92a629e01f5c85541e56a561deca0198a9393a
mahamudul hasan seber <sebermh@gmail.com>
2026-02-08 11:36:24 +0600

    More ES6

---
+++ recap.js
@@ -0,0 +1,34 @@
+// var let const 
+const tax = 5000;
+let eta = 5;
+eta = 2;
+
+const student = {name: 'seber', marks: 50}
+const friends = ['ratul', 'towhid', 'fahim' ]
+
+// default parameter
+function add(num1, num2 = 0){
+
+}
+
+// template string 
+const dynamicText = `My Tax: ${tax} and marks ${student.marks * 1.2} has
+friends: ${friends[1]}`
+
+const innerHTML = `
+<div>
+    <h1>Hello: ${friends.length}</h1>
+    <p></p>
+</div>
+`
+
+// arrow function 
+const add2= (num1, num2 = 0) => num1 + num2;
+const tenTimes = x => x * 10;
+
+// spread 
+const newFriends = [...friends, 'hasan', 'woeo']
+
+// destructuring
+const { marks: totalMarks, age = 0} = student
+console.log(totalMarks, age)
