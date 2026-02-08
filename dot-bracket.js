941959872960e0a2815f73c9a05796a5900c42c1
mahamudul hasan seber <sebermh@gmail.com>
2026-02-08 12:20:28 +0600

    Dot,Bracket,?

---
+++ dot-bracket.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'Seber',
+    1: 50,
+    'home-address': 'old dhaka',
+    marks: 90
+}
+
+// dot notation
+const studentName = student.name;
+console.log(studentName)
+
+//bracket notation
+const studentName2 = student['name']
+console.log(student['home-address'])
